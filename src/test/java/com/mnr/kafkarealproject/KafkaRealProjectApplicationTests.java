a0a8f72ece2a0be113a71536d072595bf4df7415
Mounir Bkr <miirbri1@gmail.com>
2023-06-24 18:24:04 +0100

    first commit

---
+++ src/test/java/com/mnr/kafkarealproject/KafkaRealProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.kafkarealproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
